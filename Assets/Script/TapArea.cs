5f60d5ecdb1bd117b9162501ab738f33839d8a6f
ObbiePrasetyo <obipras@gmail.com>
2021-09-10 13:39:24 +0700

    Final

---
+++ Assets/Script/TapArea.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public AudioSource GameClip;
+
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        GameClip.Play();
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+    }
+}
